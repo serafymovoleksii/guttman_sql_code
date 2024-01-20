c477646e0146ac1f48bf1b302f01c0454dffb65c
Oleksii <serafymov.oleksii@gmail.com>
2024-01-20 14:06:32 -0500

    Assignment1

--- install_profnedd_org.sql
+++ install_profnedd_org.sql
@@ -16,11 +16,12 @@
 -- Database Creation and Use:
 --
 -- Drop Database
-
+DROP DATABASE IF EXISTS `guttman_classdb_assign1`;
 -- Create database
 
+CREATE DATABASE  IF NOT EXISTS `guttman_classdb_assign1` CHARACTER SET utf8mb4  COLLATE utf8mb4_0900_ai_ci;
 -- Use database
-
+USE  `guttman_classdb_assign1`;
 
 -- Begin populating database
 
